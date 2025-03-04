7dd55d2297da1959a6e7a3f7c0dcf0f7b6be9107
chezmirak <zechkarim@gmail.com>
2025-03-04 10:36:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript für Interaktivität (optional)
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Vielen Dank für deine Nachricht! Ich werde mich so schnell wie möglich bei dir melden.');
+    // Hier könntest du den Formularinhalt an einen Server senden
+});
